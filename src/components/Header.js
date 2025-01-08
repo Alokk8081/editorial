872f2d2bcc8589309275b373f0eb284c8b88095f
Alok kumar <arav8172@gmail.com>
2025-01-09 01:49:26 +0530

    abc

--- src/components/Header.js
+++ src/components/Header.js
@@ -21,7 +21,7 @@ function Header() {
               <Link to="/about">About</Link>
             </li>
             <li className="text-sm px-4 py-1 bg-white/10 border border-white/20 text-white rounded-lg shadow-md transition-all duration-300 hover:bg-white/20 hover:border-white/40 cursor-pointer mx-2">
-              <Link to="/publications">Publications</Link>
+              <Link to="/publications">Publications abc</Link>
             </li>
             <li className="text-sm px-4 py-1 bg-white/10 border border-white/20 text-white rounded-lg shadow-md transition-all duration-300 hover:bg-white/20 hover:border-white/40 cursor-pointer mx-2">
               <Link to="/Team">Team</Link>
